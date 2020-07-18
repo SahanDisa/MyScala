1efa9a13ad2557f6e0ea38ccb5386e6fc1ce7f9c
Sahan Dissanayaka <t.sahan998@gmail.com>
2020-07-18 17:09:39 +0530

    add files via upload v3 a4

--- Assignment04/ceaser_cipher.scala
+++ Assignment04/ceaser_cipher.scala
@@ -1,14 +1,17 @@
 object ceaserCipher extends App{
+    
+    //string alphabet
     val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     //high order functions for encrypt and decrypt
     val E=(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)+key)%a.size)
 
-    val D=(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)-key)%a.size)
+    //get absolute value by .abs
+    val D=(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)-key).abs%a.size)
 
     //map reduce function for cipher
     val cipher=(algo:(Char,Int,String)=>Char,s:String,key:Int,a:String)=>s.map(algo(_,key,a))
 
-    var s = "abcd"
+    var s = "abcdxyz"
     //fewer tests
     val ct=cipher(E,s,5,alphabet)
     val pt=cipher(D,ct,5,alphabet)
