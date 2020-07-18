b1095390647b481090bf994ed4f18b1655e7d7bd
Sahan Dissanayaka <t.sahan998@gmail.com>
2020-07-18 12:44:02 +0530

    add files via upload for a4

---
+++ Assignment04/ceaser_cipher.scala
@@ -0,0 +1,24 @@
+object ceaserCipher extends App{
+    val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    //high order functions for encryption and decrypt
+    val E=(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)+key)%a.size)
+
+    val D=(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)-key)%a.size)
+
+    //map reduce functional
+    val cipher=(algo:(Char,Int,String)=>Char,s:String,key:Int,a:String)=>s.map(algo(_,key,a))
+
+    var s = "abcd"
+    //fewer tests
+    val ct=cipher(E,s,5,alphabet)
+    val pt=cipher(D,ct,5,alphabet)
+    println(ct)
+    println(pt)
+
+    //another unit test
+    s = "scala"
+    val encrypt = cipher(E,s,3,alphabet)
+    val decrypt = cipher(D,encrypt,3,alphabet)
+    println(encrypt)
+    println(decrypt)
+}
